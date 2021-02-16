aaac1e1fa1f993cf1b0d3323d0b029e04114aa0b
AmirGHUB <891205@cognizant.com>
2021-02-16 00:23:41 -0600

    added back h2

--- src/main/java/com/TDDSpringDockerHeroku/RestAPITDD/config/DatabaseConfig.java
+++ src/main/java/com/TDDSpringDockerHeroku/RestAPITDD/config/DatabaseConfig.java
@@ -17,6 +17,9 @@ public class DatabaseConfig {
     public DataSource dataSource() throws URISyntaxException {
         URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
+        //databse_url = postgres://ltlyzhrwpoecqh:e277c6c08f2af0720ccd8c2f3c326b84ae214021da6c46f20a51b8e0cc2f9f2e
+        //                    @ec2-54-90-13-87.compute-1.amazonaws.com:5432/d891q0kl4a5a9t
+
         String username = dbUri.getUserInfo().split(":")[0];
         String password = dbUri.getUserInfo().split(":")[1];
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
